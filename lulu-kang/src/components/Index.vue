e71cf0aaaf42c28af9a530ea0505ce6590c9444e
lulu-kang <dalkang.hy@gmail.com>
2021-01-13 02:07:41 +0900

    feat: basic settings

---
+++ lulu-kang/src/components/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <h1>Index page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+</style>
